e5db690684da4fad568c5afa454e356974facb92
thapelolebea46 <thapelolebea46@gmail.com>
2025-10-30 20:32:30 +0200

    Update Navbar.jsx
    I used the <Link> instead of <a> tag to make sure my page navigates to the next page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
 export default function Navbar({ label = "Home", path = "/" }) {
   return (
     <nav className="navbar">
-      <a href={path} className="nav-link">
+      <Link to={path} className="nav-link">
         {label}
-      </a>
+      </Link>
     </nav>
   );
 }
